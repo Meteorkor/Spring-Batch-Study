8b9276feb53e5fc172b7a0aeab4e93ed0244649a
doomgreen <meteor.kor@gmail.com>
2020-07-15 18:43:46 +0900

    regen

---
+++ src/test/java/com/meteor/batch/SpringBatchStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.meteor.batch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBatchStudyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
