b7aeca477e97452c2f8f50d1acc5c8ab315c25dd
unseok.kim <unseok.kim@linecorp.com>
2022-09-13 22:15:30 +0900

    add PartitionerStepJobConfig, partitionerStepJobStep2

--- src/test/java/com/meteor/batch/job/parallel/PartitionerStepJobConfig.java
+++ src/test/java/com/meteor/batch/job/parallel/PartitionerStepJobConfig.java
@@ -2,6 +2,7 @@ package com.meteor.batch.job.parallel;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.stream.IntStream;
 
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
@@ -22,9 +23,17 @@ import lombok.RequiredArgsConstructor;
 @Configuration
 @RequiredArgsConstructor
 public class PartitionerStepJobConfig {
+    public static final String CALL_CNT_KEY = "CALL_CNT_KEY";
+
     public static final String JOB_NAME = "partitionerStepJob";
+
     public static final String STEP1_NAME = "partitionerStepJobStep1";
-    public static final String STEP1_CALL_CNT = "STEP1_CALL_CNT";
+    public static final String STEP1_INNER = STEP1_NAME + "_inner";
+    public static final String STEP1_CONTEXT_1_NAME = "context1Name";
+
+    public static final String STEP2_NAME = "partitionerStepJobStep2";
+    public static final String STEP2_INNER = STEP2_NAME + "_inner";
+    public static final int STEP2_CNT = 10;
 
     private final JobBuilderFactory jobBuilderFactory;
     private final StepBuilderFactory stepBuilderFactory;
@@ -34,38 +43,70 @@ public class PartitionerStepJobConfig {
     public Job partitionerStepJob() {
         return jobBuilderFactory.get(JOB_NAME)
                                 .start(partitionerStepJobStep1())
+                                .next(partitionerStepJobStep2())
                                 .build();
     }
 
     @Bean
     public Step partitionerStepJobStep1() {
         return stepBuilderFactory.get(STEP1_NAME)
-                                 .partitioner(STEP1_NAME + "Partitioner", partitioner())
+                                 .partitioner(STEP1_NAME + "Partitioner", step1Partitioner())
                                  .step(partitionerStepJobStep1Inner())
                                  .build();
     }
 
     @Bean
     public Step partitionerStepJobStep1Inner() {
-        return stepBuilderFactory.get(STEP1_NAME + "_inner")
+        return stepBuilderFactory.get(STEP1_INNER)
                                  .tasklet((stepContribution, chunkContext) -> {
                                      final ExecutionContext executionContext =
                                              stepContribution.getStepExecution()
                                                              .getExecutionContext();
 
-                                     //not parallel
-                                     //TODO how to parallel context put
-                                     executionContext.putInt(STEP1_CALL_CNT,
-                                                             executionContext.getInt(STEP1_CALL_CNT, 0) + 1);
+                                     executionContext.putInt(CALL_CNT_KEY,
+                                                             executionContext.getInt(CALL_CNT_KEY, 0) + 1);
 
                                      return RepeatStatus.FINISHED;
                                  }).build();
     }
 
-    public Partitioner partitioner() {
+    public Partitioner step1Partitioner() {
         return gridSize -> {
             Map<String, ExecutionContext> map = new HashMap<>();
-            map.put("test_1", new ExecutionContext());
+            map.put(STEP1_CONTEXT_1_NAME, new ExecutionContext());
+            return map;
+        };
+    }
+
+    @Bean
+    public Step partitionerStepJobStep2() {
+        return stepBuilderFactory.get(STEP2_NAME)
+                                 .partitioner(STEP2_NAME + "Partitioner", step2Partitioner())
+                                 .step(partitionerStepJobStep2Inner())
+                                 .build();
+    }
+
+    @Bean
+    public Step partitionerStepJobStep2Inner() {
+        return stepBuilderFactory.get(STEP2_INNER)
+                                 .tasklet((stepContribution, chunkContext) -> {
+                                     final ExecutionContext executionContext =
+                                             stepContribution.getStepExecution()
+                                                             .getExecutionContext();
+
+                                     executionContext.putInt(CALL_CNT_KEY,
+                                                             executionContext.getInt(CALL_CNT_KEY, 0) + 1);
+
+                                     return RepeatStatus.FINISHED;
+                                 }).build();
+    }
+
+    public Partitioner step2Partitioner() {
+        return gridSize -> {
+            Map<String, ExecutionContext> map = new HashMap<>();
+            IntStream.range(0, STEP2_CNT).forEach(n ->
+                                                          map.put("context_" + n, new ExecutionContext())
+            );
             return map;
         };
     }
--- src/test/java/com/meteor/batch/job/parallel/PartitionerStepJobTest.java
+++ src/test/java/com/meteor/batch/job/parallel/PartitionerStepJobTest.java
@@ -2,6 +2,7 @@ package com.meteor.batch.job.parallel;
 
 import org.junit.Before;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.batch.core.BatchStatus;
 import org.springframework.batch.core.ExitStatus;
@@ -34,14 +35,15 @@ public class PartitionerStepJobTest {
         this.jobRepositoryTestUtils.removeJobExecutions();
     }
 
+    @DisplayName("단건 executionContext")
     @Test
-    void jobLauncherTestUtilsLaunchJobParametersFailTest() throws Exception {
+    void jobLauncherPartitionerStep1Test() throws Exception {
         final JobParameters jobParameters = new JobParametersBuilder().addLong(RetryJobConfig.FAIL_CNT, 10L)
                                                                       .toJobParameters();
         final JobExecution jobExecution = jobLauncherTestUtils.launchJob(jobParameters);
 
-        //TODO context 체크
-        String partitionerContext = PartitionerStepJobConfig.STEP1_NAME + "_inner:test_1";
+        String partitionerContext = PartitionerStepJobConfig.STEP1_INNER
+                                    + ":" + PartitionerStepJobConfig.STEP1_CONTEXT_1_NAME;
 
         final StepExecution stepExecution1 = jobExecution.getStepExecutions().stream().filter(
                 stepExecution -> partitionerContext.equals(stepExecution.getStepName())
@@ -50,7 +52,30 @@ public class PartitionerStepJobTest {
         Assertions.assertEquals(BatchStatus.COMPLETED, jobExecution.getStatus());
         Assertions.assertEquals(ExitStatus.COMPLETED.getExitCode(), jobExecution.getExitStatus().getExitCode());
         Assertions.assertEquals(1, stepExecution1.getExecutionContext()
-                                                 .getInt(PartitionerStepJobConfig.STEP1_CALL_CNT));
+                                                 .getInt(PartitionerStepJobConfig.CALL_CNT_KEY));
+    }
+
+    @DisplayName("다건 executionContext")
+    @Test
+    void jobLauncherPartitionerStep2Test() throws Exception {
+        final JobParameters jobParameters = new JobParametersBuilder().addLong(RetryJobConfig.FAIL_CNT, 10L)
+                                                                      .toJobParameters();
+        final JobExecution jobExecution = jobLauncherTestUtils.launchJob(jobParameters);
+
+        final long count = jobExecution.getStepExecutions().stream().filter(
+                stepExecution -> stepExecution.getStepName().startsWith(PartitionerStepJobConfig.STEP2_INNER)
+        ).count();
+
+        Assertions.assertEquals(BatchStatus.COMPLETED, jobExecution.getStatus());
+        Assertions.assertEquals(ExitStatus.COMPLETED.getExitCode(), jobExecution.getExitStatus().getExitCode());
+        Assertions.assertEquals(PartitionerStepJobConfig.STEP2_CNT, count);
+    }
+
+    //TODO
+    @DisplayName("다건 chunked")
+    @Test
+    void jobLauncherPartitionerStep3Test() throws Exception {
+
     }
 
 }
