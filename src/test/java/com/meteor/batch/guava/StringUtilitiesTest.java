743636269a0cf10a938454c73b9bf6067df76216
unseok.kim <unseok.kim@linecorp.com>
2022-09-16 22:13:50 +0900

    edit StringUtilitiesTest, listNullTest, ifTest, mapJoiner

--- src/test/java/com/meteor/batch/guava/StringUtilitiesTest.java
+++ src/test/java/com/meteor/batch/guava/StringUtilitiesTest.java
@@ -1,11 +1,36 @@
 package com.meteor.batch.guava;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import com.google.common.base.Joiner;
 
 public class StringUtilitiesTest {
+
+    @Test
+    void listNullTest() {
+        List<String> list = null;
+
+        Assertions.assertThrows(NullPointerException.class, () -> {
+            for (String text : list) {
+                Assertions.fail();
+            }
+        });
+    }
+
+    @Test
+    void ifTest() {
+        if (false && false || true) {
+
+        } else {
+            Assertions.fail();
+        }
+    }
 
     @Test
     void joinerTest() {
@@ -33,10 +58,21 @@ public class StringUtilitiesTest {
         }
     }
 
-    //TODO
-//    @Test
-//    void mapJoiner(){
-//
-//    }
+    @Test
+    void mapJoiner() {
+        Map<String, String> map = new HashMap<>();
+        map.put("key1", "val1");
+        map.put("key2", "val2");
+        map.put("key3", "val3");
+
+        final String keyValueSeparator = ":";
+        final String separator = ",";
+        final String joinStr = Joiner.on(separator).withKeyValueSeparator(keyValueSeparator).join(map);
+
+        //"key1:val1,key2:val2,key3:val3"
+        Assertions.assertEquals(
+                map.entrySet().stream().map(entry -> entry.getKey() + keyValueSeparator + entry.getValue())
+                   .collect(Collectors.joining(separator)), joinStr);
+    }
 
 }
