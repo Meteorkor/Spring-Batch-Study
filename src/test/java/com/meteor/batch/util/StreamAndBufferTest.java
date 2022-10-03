8e63fd5232a34fa0e2d1ff8b0df199b1d3f349c0
unseok.kim <unseok.kim@linecorp.com>
2022-10-03 22:45:01 +0900

    add writePerformanceTest_smallString, writePerformanceTest_BigString

--- src/test/java/com/meteor/batch/util/StreamAndBufferTest.java
+++ src/test/java/com/meteor/batch/util/StreamAndBufferTest.java
@@ -1,14 +1,17 @@
 package com.meteor.batch.util;
 
 import java.io.BufferedOutputStream;
+import java.io.BufferedWriter;
 import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileOutputStream;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.OutputStreamWriter;
 import java.nio.ByteBuffer;
 import java.nio.channels.FileChannel;
 import java.nio.file.Files;
+import java.nio.file.StandardOpenOption;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
@@ -109,13 +112,79 @@ public class StreamAndBufferTest {
         System.out.println("outputStreamWriterTime : " + outputStreamWriterTime);
     }
 
+    //small string repeat
     @Test
-    void writePerformanceTest() throws IOException {
+    void writePerformanceTest_smallString() throws IOException {
         File temp = File.createTempFile("prefix", "suffix");
 
         String output = "text";
 //        int repeatCnt = 10_000_000;
         int repeatCnt = 1_000_000;
+//        int repeatCnt = 1000;
+
+        final long fileWriterTime = TestTimeUtils.processTime(() -> {
+            try (final FileWriter fileWriter = new FileWriter(temp);) {
+                IntStream.range(0, repeatCnt).forEach(n -> {
+                    try {
+                        fileWriter.write(output);
+                    } catch (IOException ignore) {
+                    }
+                });
+
+            } catch (IOException ignore) {
+            }
+        });
+
+        //fileWriterTime : 196
+//        fileWriterTime : 164
+        System.out.println("fileWriterTime : " + fileWriterTime);
+        Assertions.assertEquals(Strings.repeat(output, repeatCnt), Files.readAllLines(temp.toPath())
+                                                                        .stream()
+                                                                        .collect(Collectors.joining()));
+        temp.delete();
+
+        final long bufferedWriterTime = TestTimeUtils.processTime(() -> {
+            try (final BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(temp));) {
+                IntStream.range(0, repeatCnt).forEach(n -> {
+                    try {
+                        bufferedWriter.write(output);
+                    } catch (IOException ignore) {
+                    }
+                });
+
+            } catch (IOException ignore) {
+            }
+        });
+
+        //bufferedWriterTime : 42
+        //bufferedWriterTime : 46
+        System.out.println("bufferedWriterTime : " + bufferedWriterTime);
+        Assertions.assertEquals(Strings.repeat(output, repeatCnt), Files.readAllLines(temp.toPath())
+                                                                        .stream()
+                                                                        .collect(Collectors.joining()));
+        temp.delete();
+
+        final long fileBufferedWriterTime = TestTimeUtils.processTime(() -> {
+            try (final BufferedWriter fileBufferedWriter = Files.newBufferedWriter(temp.toPath());) {
+                IntStream.range(0, repeatCnt).forEach(n -> {
+                    try {
+                        fileBufferedWriter.write(output);
+                    } catch (IOException ignore) {
+                    }
+                });
+
+            } catch (IOException ignore) {
+            }
+        });
+
+        //fileBufferedWriterTime : 57
+//        fileBufferedWriterTime : 42
+        //확실히 buffered가 빠름, string 저장시에는 bytes 를 넘겨야하는 outpustream 보다는 string 그대로 넘길수 있는 writer 활용
+        System.out.println("fileBufferedWriterTime : " + fileBufferedWriterTime);
+        Assertions.assertEquals(Strings.repeat(output, repeatCnt), Files.readAllLines(temp.toPath())
+                                                                        .stream()
+                                                                        .collect(Collectors.joining()));
+        temp.delete();
 
         final long fileOutputStreamTime = TestTimeUtils.processTime(() -> {
             try (final FileOutputStream fileOutputStream = new FileOutputStream(temp)) {
@@ -159,9 +228,6 @@ public class StreamAndBufferTest {
                                                                         .collect(Collectors.joining()));
         temp.delete();
 
-//TODO        Files.newBufferedWriter()
-
-//        final ByteBuffer allocate = ByteBuffer.allocate(1024);
         final long channelWriteTime = TestTimeUtils.processTime(() -> {
             try (final FileChannel fileChannel = new FileOutputStream(temp).getChannel()) {
                 IntStream.range(0, repeatCnt).forEach(n -> {
@@ -179,16 +245,194 @@ public class StreamAndBufferTest {
             } catch (IOException ignore) {
             }
         });
+
 //        channelWriteTime : 3414, allocate.clear();
         System.out.println("channelWriteTime : " + channelWriteTime);
         Assertions.assertEquals(Strings.repeat(output, repeatCnt), Files.readAllLines(temp.toPath())
                                                                         .stream()
                                                                         .collect(Collectors.joining()));
+        temp.delete();
+
+        final long filesWriteTime = TestTimeUtils.processTime(() -> {
+
+            IntStream.range(0, repeatCnt).forEach(n -> {
+                try {
+                    Files.write(temp.toPath(), output.getBytes(), StandardOpenOption.CREATE,
+                                StandardOpenOption.APPEND);
+                } catch (IOException ignore) {
+                    ignore.printStackTrace();
+                }
+            });
+
+        });
+
+        //아주아주 오래 시간이 소요됨..
+        //같은 파일에 여러번쓸때는 Files.write는 사용하지 않도록.. 계속 열고 닫고 등등의 체크가 있으니 상당히 성능이 안나오는것으로 보임
+        System.out.println("filesWriteTime : " + filesWriteTime);
+        Assertions.assertEquals(Strings.repeat(output, repeatCnt), Files.readAllLines(temp.toPath())
+                                                                        .stream()
+                                                                        .collect(Collectors.joining()));
+        temp.delete();
+
         //확연히 bufferedOutputStream가 빠름, TODO Channel에서 어떻게 Buffered 적용하는지 확인
 //        fileOutputStreamTime : 3219
 //        bufferedOutputStreamTime : 62
 //        channelWriteTime : 3622
 
+    }
+
+    @Test
+    void writePerformanceTest_BigString() throws IOException {
+        File temp = File.createTempFile("prefix", "suffix");
+
+//        int repeatCnt = 10;
+        int repeatCnt = 10_000_000;//300MB
+        String output = Strings.repeat("text", repeatCnt);
+
+        final long fileWriterTime = TestTimeUtils.processTime(() -> {
+            try (final FileWriter fileWriter = new FileWriter(temp);) {
+                fileWriter.write(output);
+            } catch (IOException ignore) {
+            }
+        });
+
+        //fileWriterTime : 209
+        System.out.println("fileWriterTime : " + fileWriterTime);
+        Assertions.assertEquals(output, Files.readAllLines(temp.toPath())
+                                             .stream()
+                                             .collect(Collectors.joining()));
+        temp.delete();
+
+        final long bufferedWriterTime = TestTimeUtils.processTime(() -> {
+            try (final BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(temp));) {
+                bufferedWriter.write(output);
+            } catch (IOException ignore) {
+            }
+        });
+
+        //bufferedWriterTime : 146
+        System.out.println("bufferedWriterTime : " + bufferedWriterTime);
+        Assertions.assertEquals(output, Files.readAllLines(temp.toPath())
+                                             .stream()
+                                             .collect(Collectors.joining()));
+        temp.delete();
+
+        final long fileBufferedWriterTime = TestTimeUtils.processTime(() -> {
+            try (final BufferedWriter fileBufferedWriter = Files.newBufferedWriter(temp.toPath());) {
+                fileBufferedWriter.write(output);
+            } catch (IOException ignore) {
+            }
+        });
+
+        //fileBufferedWriterTime : 109
+        System.out.println("fileBufferedWriterTime : " + fileBufferedWriterTime);
+        Assertions.assertEquals(output, Files.readAllLines(temp.toPath())
+                                             .stream()
+                                             .collect(Collectors.joining()));
+        temp.delete();
+
+        final long fileOutputStreamTime = TestTimeUtils.processTime(() -> {
+            try (final FileOutputStream fileOutputStream = new FileOutputStream(temp)) {
+                fileOutputStream.write(output.getBytes());
+            } catch (IOException ignore) {
+            }
+        });
+
+        System.out.println("fileOutputStreamTime : " + fileOutputStreamTime);
+        Assertions.assertEquals(output, Files.readAllLines(temp.toPath())
+                                             .stream()
+                                             .collect(Collectors.joining()));
+        temp.delete();
+
+        final long bufferedOutputStreamTime = TestTimeUtils.processTime(() -> {
+            try (final BufferedOutputStream bufferedOutputStream = new BufferedOutputStream(
+                    new FileOutputStream(temp));) {
+                bufferedOutputStream.write(output.getBytes());
+            } catch (IOException ignore) {
+            }
+        });
+
+        //bufferedOutputStreamTime : 49
+        System.out.println("bufferedOutputStreamTime : " + bufferedOutputStreamTime);
+
+        Assertions.assertEquals(output, Files.readAllLines(temp.toPath())
+                                             .stream()
+                                             .collect(Collectors.joining()));
+        temp.delete();
+
+        final long channelWriteTime = TestTimeUtils.processTime(() -> {
+            try (final FileChannel fileChannel = new FileOutputStream(temp).getChannel()) {
+
+                try {
+                    final ByteBuffer allocate = ByteBuffer.allocate(output.length());
+                    allocate.put(output.getBytes());
+                    allocate.flip();
+                    fileChannel.write(allocate);
+//                    final ByteBuffer allocate = ByteBuffer.allocate(1024);
+//                    allocate.put(output.getBytes());
+//                    allocate.flip();
+//                    fileChannel.write(allocate);
+                } catch (IOException ignore) {
+                }
+
+            } catch (IOException ignore) {
+            }
+        });
+
+        //channelWriteTime : 59
+        System.out.println("channelWriteTime : " + channelWriteTime);
+        Assertions.assertEquals(output, Files.readAllLines(temp.toPath())
+                                             .stream()
+                                             .collect(Collectors.joining()));
+        temp.delete();
+
+        final long channelDirectWriteTime = TestTimeUtils.processTime(() -> {
+            try (final FileChannel fileChannel = new FileOutputStream(temp).getChannel()) {
+
+                try {
+                    final ByteBuffer allocate = ByteBuffer.allocateDirect(output.length());
+                    allocate.put(output.getBytes());
+                    allocate.flip();
+                    fileChannel.write(allocate);
+                } catch (IOException ignore) {
+                }
+
+            } catch (IOException ignore) {
+            }
+        });
+
+        System.out.println("channelDirectWriteTime : " + channelDirectWriteTime);
+        Assertions.assertEquals(output, Files.readAllLines(temp.toPath())
+                                             .stream()
+                                             .collect(Collectors.joining()));
+        temp.delete();
+
+        final long filesWriteTime = TestTimeUtils.processTime(() -> {
+
+            try {
+                Files.write(temp.toPath(), output.getBytes());
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        });
+
+        //filesWriteTime : 116
+        System.out.println("filesWriteTime : " + filesWriteTime);
+        Assertions.assertEquals(output, Files.readAllLines(temp.toPath())
+                                             .stream()
+                                             .collect(Collectors.joining()));
+        temp.delete();
+
+//        fileWriterTime : 174
+//        bufferedWriterTime : 172
+//        fileBufferedWriterTime : 124
+//        fileOutputStreamTime : 66
+//        bufferedOutputStreamTime : 53
+//        channelWriteTime : 66
+//        channelDirectWriteTime : 87
+
+        //큰파일을 바로 쓸때는 buffered는 조금 비효율적인듯..
+        //하지만, 보통 파일에 쓸때는 메모리에 데이터를 많이 올리지 않고 나눠서 올리기 때문에, Buffered는 충분히 활용해야함
 
     }
 
